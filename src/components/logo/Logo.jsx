fdfc0436be4e42bbfc5dd6a23c0c957739f40c45
Jesus David Nazareno Cardona <DnNzareno@gmail.com>
2022-12-08 23:02:20 +0100

    fixed logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -2,7 +2,7 @@ import IconLogo from "../../assets/logo.png";
 const Logo = () => {
   return (
     <div className="flex items-center justify-start">
-      <img  src={IconLogo} className=" h-12 w-12" alt="Vite logo" />
+      <img  src={IconLogo} className=" h-12 w-12" alt="Prooflist logo" />
       <span className="uppercase font-bold   text-xl">PROOFLIST</span>
     </div>
   );
