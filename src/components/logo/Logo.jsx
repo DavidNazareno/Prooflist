013905480a3de4c331c0a3d24873d97ec3aaacc1
Jesus David Nazareno Cardona <DnNzareno@gmail.com>
2022-12-08 23:00:01 +0100

    up

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,7 +1,8 @@
+import IconLogo from "../../assets/logo.png";
 const Logo = () => {
   return (
     <div className="flex items-center justify-start">
-      <img  src="/logo.png" className=" h-12 w-12" alt="Vite logo" />
+      <img  src={IconLogo} className=" h-12 w-12" alt="Vite logo" />
       <span className="uppercase font-bold   text-xl">PROOFLIST</span>
     </div>
   );
